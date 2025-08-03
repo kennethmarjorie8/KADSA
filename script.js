83fc344334be70a7ade7f6dfdc1749fa9b340a87
kennethmarjorie8 <marjoriefiallos95@gmail.com>
2025-08-03 17:12:11 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function prevImage() {
   index = (index - 1 + images.length) % images.length;
   showImage();
 }
+
+___________________________________________________________________
+function openModal(src) {
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modal-img');
+  modal.style.display = 'flex';
+  modalImg.src = src;
+}
+
+function closeModal() {
+  document.getElementById('modal').style.display = 'none';
+}
+
+
+
